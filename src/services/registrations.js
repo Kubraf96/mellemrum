6d038ef61bdc77819036e0455e3c1a95570b6952
Kubraf96 <kubrafidan25@hotmail.com>
2026-09-01 20:26:51 +0200

    importér users fra supabase

--- src/services/registrations.js
+++ src/services/registrations.js
@@ -1,5 +1,5 @@
 // Henter de funktioner vi skal bruge til at snakke med Supabase
-import { registrationsUrl, request } from "../lib/supabase";
+import { registrationsUrl, usersUrl, request } from "../lib/supabase";
 
 // Gemmer en ny tilmelding i registrations-tabellen
 export function createRegistration(registration) {
